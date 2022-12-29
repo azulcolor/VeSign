dc6d4635719a66de6806b8e516dd6aa760bf0995
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 <lealpastelin@gmail.com>
2022-12-29 15:23:07 -0500

    optimazed

---
+++ view/components/signDocument/sign/component.jsx
@@ -0,0 +1,28 @@
+import SignatureCanvas from 'react-signature-canvas'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+import style from '../../../styles/signDocument/canvas.module.css'
+
+export default function Component({ canvas, screenWidth, setIsSigned }) {
+  const clear = () => {
+    canvas.current.clear()
+    setIsSigned(false)
+  }
+  return (
+    <>
+      <SignatureCanvas
+        ref={canvas}
+        canvasProps={{
+          className: 'signatureCanvas',
+        }}
+        maxWidth={screenWidth.current < 768 ? 0.9 : 1.2}
+        minWidth={screenWidth.current < 768 ? 0.7 : 1}
+        onBegin={() => setIsSigned(true)}
+      />
+
+      <div className={style.restart}>
+        <DeleteIcon onClick={clear} className={style.button} />
+      </div>
+    </>
+  )
+}
